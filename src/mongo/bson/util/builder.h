6548bd862d189a14fc116b7ff2ab401c66e5714a
Ma Ni Sh <manis.sainju@gmail.com>
2016-11-09 15:40:44 +0545

    Increased BSON size limit to 28MB
    The bson size limit is increased to 28 MB to overcome Document exceeds maximal allowed bson size limit

--- src/mongo/bson/util/builder.h
+++ src/mongo/bson/util/builder.h
@@ -60,7 +60,7 @@ namespace mongo {
      update $push (append) operation
      various db.eval() type operations
 */
-const int BSONObjMaxUserSize = 16 * 1024 * 1024;
+const int BSONObjMaxUserSize = 24 * 1024 * 1024;
 
 /*
    Sometimes we need objects slightly larger - an object in the replication local.oplog
